5f2270211ce7fc4f1223680d7defa21a11e4d7d3
amir mohammad mehrabi moghadam <amir.mehrabi93@gmail.com>
2019-02-25 23:13:01 +0330

    Add project files.

---
+++ fileuploadjsnetcore/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using fileuploadjsnetcore.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace fileuploadjsnetcore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _hostingEnvironment;
+        public HomeController(IHostingEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+           
+
+        }
+
+       [HttpPost]
+        public IActionResult IndexPost()
+        {
+            var files = Request.Form.Files;
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+            var filePath = Path.GetTempFileName();
+            foreach (var formFile in files)
+            {
+
+                if (formFile.Length > 0)
+                {
+                    string guidNewGuid = Guid.NewGuid().ToString();
+                    filePath = Path.Combine(uploads, guidNewGuid + Path.GetFileName(formFile.FileName));
+
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        formFile.CopyTo(fileStream);
+                    }
+                }
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
